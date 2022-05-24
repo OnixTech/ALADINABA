37a7e8a0c78fac7a1877e1ec9aec049d61f3364c
Chloe Cartron <chloe.cartron@gmail.com>
2022-05-24 15:11:34 +0200

    fix some bugs with the show page and add book button

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.destroy_all
 Carpet.destroy_all
+User.destroy_all
 
-puts "create 1 user"
 user1 = User.new(
   email: "user@mail.com",
   password: "123456"
@@ -25,7 +24,7 @@ name = ["Persian carpet", "Cat carpet", "Hole carpet", "Forest carpet"]
 description = ["Fuck you Tony!", "Better not rent it", "Not too bad, not too bad", "Perfect condition", "good condition, like 0km"]
 address = ["catch me if you can", "High Avenue Z, next to the bridge", "Kulamalab 56, Istanbul", "Badstrasse 43, 10345 Berlin", "Koloniestrasse 140, 13359 Berlin"]
 
-20.times do |i|
+20.times do
     Carpet.create(
     name: name.sample,
     description: description.sample,
@@ -33,6 +32,6 @@ address = ["catch me if you can", "High Avenue Z, next to the bridge", "Kulamala
     condition: "like new",
     price: 12,
     available: true,
-    user_id: 1
-)
-  end
+    user: user1
+  )
+end
