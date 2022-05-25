405e0f90abdb153ed3fbb71f4b7847d6a419a0dd
Julius Blezinger <jgoldappel@gmail.com>
2022-05-25 11:45:05 +0200

    changed design of dashboard and bookings/new and added links to home and dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "pages#home"
+  root to: "carpets#index"
 end
