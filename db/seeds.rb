5fcabd245bbd4663e19e232865312725d5e2d4ed
Luciano Petillo <luciano.prnl@gmail.com>
2022-05-25 13:09:17 +0200

    Photos added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,9 +21,10 @@ user2.save!
 name = ["Persian carpet", "Cat carpet", "Hole carpet", "Forest carpet"]
 description = ["Fuck you Tony!", "Better not rent it", "Not too bad, not too bad", "Perfect condition", "good condition, like 0km"]
 address = ["catch me if you can", "High Avenue Z, next to the bridge", "Kulamalab 56, Istanbul", "Badstrasse 43, 10345 Berlin", "Koloniestrasse 140, 13359 Berlin"]
+file = URI.open("https://m.media-amazon.com/images/I/71ErLr+On5L._AC_SL1500_.jpg")
 
 20.times do
-    Carpet.create(
+    carpet = Carpet.new(
     name: name.sample,
     description: description.sample,
     address: address.sample,
@@ -32,5 +33,6 @@ address = ["catch me if you can", "High Avenue Z, next to the bridge", "Kulamala
     available: true,
     user: user1
   )
+  carpet.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+  carpet.save
 end
-
